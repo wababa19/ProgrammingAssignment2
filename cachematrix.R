27e52731110a180e92e28b62310bb3fed2a038a0
wababa19 <54188992+wababa19@users.noreply.github.com>
2019-08-17 09:34:08 +0200

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Function that creates and cache the inverse matrix
 
-## Write a short comment describing this function
+
+## The function load the package matrixcalc, which is collection
+## of functions for matrix calculations. We need the functiom 
+## matrix.inverse in order to calculate inverse of matrix
+## Before computations, the function check the singularity 
+## of matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  require(matrixcalc)
+  # check if the matrix is singular, i.e. irreversible
+  if(is.singular.matrix(x) == TRUE){
+    stop('Matrix in irreversible !')}
+  minv<- NULL
+  set <- function(y) {
+    x <<- y
+    minv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) minv <<- inverse
+  getinverse <- function() minv
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function returns inverse matrix
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve<- function(x, ...) {
+  minv <- x$getinverse()
+  if(!is.null(minv)) {
+    message("getting cached inverse matrix")
+    return(minv)
+  }
+  data <- x$get()
+  minv <- solve(data, ...)
+  x$setinverse(minv)
+  minv
 }
